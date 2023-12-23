a33e9f1ceb87b8c2c5c669cdbd415b37b235c16c
abdul-nishar <sheikhabdul285@gmail.com>
2023-12-23 17:23:45 +0530

    React Essentials

---
+++ reactEssentials/src/components/CoreConcept.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default function CoreConcept({ title, description, image }) {
+  return (
+    <li>
+      <img src={image} alt={title}></img>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
