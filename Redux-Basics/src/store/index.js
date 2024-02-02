03fe4fa03c86820b1b186585cb358545e5410f5b
abdul-nishar <sheikhabdul285@gmail.com>
2024-02-02 23:01:07 +0530

    Basic Redux

---
+++ Redux-Basics/src/store/index.js
@@ -0,0 +1,45 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const initialCounterState = { counter: 0, showToggle: true };
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter += action.payload;
+    },
+    toggleCounter(state) {
+      state.showToggle = !state.showToggle;
+    },
+  },
+});
+
+const initialAuthState = { isAuthenticated: false };
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+});
+
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+
+export default store;
